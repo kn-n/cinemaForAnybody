0b51638def31d61be3e37eab3a1561f4a732809c
lisiyytka <gritroma@yandex.ru>
2021-04-18 18:55:38 +0500

    My life is scary

--- src/main/java/com/example/cinema/controller/DatabaseController.java
+++ src/main/java/com/example/cinema/controller/DatabaseController.java
@@ -1,10 +1,87 @@
 package com.example.cinema.controller;
 
+import com.example.cinema.domain.AllFilms;
+import com.example.cinema.domain.Film;
+import com.example.cinema.domain.Item;
+import com.example.cinema.repos.FilmRepo;
+import com.example.cinema.repos.MessageRepo;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+
+
 
 @Controller
 public class DatabaseController {
-    public void AddFilm(){
+    public static Item item;
 
+    @Autowired
+    private FilmRepo filmRepo;
+
+    @GetMapping("/main")
+    public String main(Map<String, Object> model) throws IOException {
+
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_a3bczbcf";
+
+        URL obj = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+        connection.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        Gson g = new Gson();
+        item = g.fromJson(response.toString(), Item.class);
+
+        for (int i = 1; i<=250; i++) {
+            AllFilms aue = new AllFilms();
+            if (aue.getStringId() == null) {
+                aue.setStringId(item.items.get(i - 1).id);
+                aue.setRank(item.items.get(i - 1).rank);
+                aue.setTitle(item.items.get(i - 1).title);
+                aue.setFullTitle(item.items.get(i - 1).fullTitle);
+                aue.setYear(item.items.get(i - 1).year);
+                aue.setImage(item.items.get(i - 1).image);
+                aue.setCrew(item.items.get(i - 1).crew);
+                aue.setImDbRating(item.items.get(i - 1).imDbRating);
+                aue.setImDbRatingCount(item.items.get(i - 1).imDbRatingCount);
+                filmRepo.save(aue);
+            }
+            else break;
+        }
+
+//        for (int i = 1; i<=250; i++) {
+//            AllFilms aue = new AllFilms();
+//
+//            aue.setStringId(item.items.get(i - 1).id);
+//            aue.setRank(item.items.get(i - 1).rank);
+//            aue.setTitle(item.items.get(i - 1).title);
+//            aue.setFullTitle(item.items.get(i - 1).fullTitle);
+//            aue.setYear(item.items.get(i - 1).year);
+//            aue.setImage(item.items.get(i - 1).image);
+//            aue.setCrew(item.items.get(i - 1).crew);
+//            aue.setImDbRating(item.items.get(i - 1).imDbRating);
+//            aue.setImDbRatingCount(item.items.get(i - 1).imDbRatingCount);
+//            filmRepo.save(aue);
+//        }
+
+        model.put("films", item.items);
+
+        return "main";
     }
 }
--- src/main/java/com/example/cinema/controller/MainController.java
+++ src/main/java/com/example/cinema/controller/MainController.java
@@ -47,64 +47,6 @@ public class MainController {
         return "greeting";
     }
 
-    @GetMapping("/main")
-    public String main(Map<String, Object> model) throws IOException {
-
-        String url = "https://imdb-api.com/en/API/Top250Movies/k_a3bczbcf";
-
-        URL obj = new URL(url);
-        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
-
-        connection.setRequestMethod("GET");
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-
-        Gson g = new Gson();
-        item = g.fromJson(response.toString(), Item.class);
-
-        AllFilms aue = new AllFilms();
-
-//        for (Film i:item.items)
-//        {
-//            aue.setStringId(i.id);
-//            aue.setRank(i.rank);
-//            aue.setTitle(i.title);
-//            aue.setFullTitle(i.fullTitle);
-//            aue.setYear(i.year);
-//            aue.setImage(i.image);
-//            aue.setCrew(i.crew);
-//            aue.setImDbRating(i.imDbRating);
-//            aue.setImDbRatingCount(i.imDbRatingCount);
-//            filmRepo.save(aue);
-//        }
-
-        for (int i = 1; i<=250; i++) {
-            aue.setStringId(item.items.get(i - 1).id);
-            aue.setRank(item.items.get(i - 1).rank);
-            aue.setTitle(item.items.get(i - 1).title);
-            aue.setFullTitle(item.items.get(i - 1).fullTitle);
-            aue.setYear(item.items.get(i - 1).year);
-            aue.setImage(item.items.get(i - 1).image);
-            aue.setCrew(item.items.get(i - 1).crew);
-            aue.setImDbRating(item.items.get(i - 1).imDbRating);
-            aue.setImDbRatingCount(item.items.get(i - 1).imDbRatingCount);
-            System.out.println("Jizn voram"+(i-1));
-            filmRepo.save(aue);
-            System.out.println("Smert voram"+(i-1));
-        }
-
-        model.put("films", item.items);
-
-        return "main";
-    }
-
     @PostMapping("filter")
     public String filter(@RequestParam String filter, Map<String, Object> model){
 
